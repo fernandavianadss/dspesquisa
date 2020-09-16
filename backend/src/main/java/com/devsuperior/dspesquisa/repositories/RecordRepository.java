721be6299d317e38db0902e2be0fafd0c8d7bb73
Fernanda Viana <fernanda.vianadss@gmail.com>
2020-09-16 10:56:46 -0300

    Entities, Repositories

---
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.dspesquisa.entites.Game;
+import com.devsuperior.dspesquisa.entites.Record;
+
+public interface RecordRepository extends JpaRepository<Record, Long>{
+
+}
