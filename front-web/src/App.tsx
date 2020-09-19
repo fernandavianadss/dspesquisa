c510f6df36f551727ca2a9446a15d80c42e93e23
Fernanda Viana <fernanda.vianadss@gmail.com>
2020-09-19 18:36:21 -0300

    Navigation routes created

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Home from './Pages/Home';
-
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-       <Header />
-       <Home />
-    </div>
+    <Routes />
   );
 }
 
